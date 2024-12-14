88fb7695013711959d76a358a452de06647ae6e6
Rafael Freitas <raffreitasx@outlook.com>
2024-12-14 18:27:59 -0300

    feat: adding automapper and password encrypter

---
+++ src/MyRecipes.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyRecipes.Communication.Requests;
+
+namespace MyRecipes.Application.Services.AutoMapper;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        RequestToDomain();
+    }
+
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+            .ForMember(destination => destination.Password, option => option.Ignore());
+    }
+}

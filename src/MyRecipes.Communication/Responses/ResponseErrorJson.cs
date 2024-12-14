834cd02c5eb91783db8204e6d8fd5d1d15006776
Rafael Freitas <raffreitasx@outlook.com>
2024-12-14 17:59:45 -0300

    feat: adding response error json

---
+++ src/MyRecipes.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRecipes.Communication.Responses;
+
+public class ResponseErrorJson
+{
+    public IList<string> Errors { get; set; }
+
+    public ResponseErrorJson(IList<string> errors) => Errors = errors;
+
+    public ResponseErrorJson(string error) => Errors = [error];
+
+}

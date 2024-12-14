6f85a4cc8e0ef5de4a8575f958af263e126c07b7
Rafael Freitas <raffreitasx@outlook.com>
2024-12-14 18:10:31 -0300

    feat: adding user entity and entity base

---
+++ src/MyRecipes.Domain/Entities/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipes.Domain.Entities;
+
+public abstract class EntityBase
+{
+    public long Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}

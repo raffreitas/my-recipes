44cadbea481d35128da2d579c8285ed00c938c97
Rafael Freitas <raffreitasx@outlook.com>
2024-12-14 19:28:53 -0300

    feat: adding extension method to get the database connection

---
+++ src/MyRecipes.Infrastructure/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MyRecipes.Infrastructure.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static string GetDatabaseConnectionString(this IConfiguration configuration)
+    {
+        return configuration.GetConnectionString("DefaultConnection")!;
+    }
+}

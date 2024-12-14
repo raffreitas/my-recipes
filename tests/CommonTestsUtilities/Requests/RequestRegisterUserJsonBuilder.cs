1a3c7747f090c03bd4e2ef81ffbb633d33fe0885
Rafael Freitas <raffreitasx@outlook.com>
2024-12-14 20:08:44 -0300

    tests: adding register user validation tests

---
+++ tests/CommonTestsUtilities/Requests/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using MyRecipes.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+
+public class RequestRegisterUserJsonBuilder
+{
+    public static RequestRegisterUserJson Build(int passwordLength = 10)
+    {
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(request => request.Name, (f) => f.Person.FirstName)
+            .RuleFor(request => request.Email, (f, r) => f.Internet.Email(r.Name))
+            .RuleFor(request => request.Password, (f) => f.Internet.Password(passwordLength));
+    }
+}
---
+++ tests/Validators.Tests/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,90 @@
+﻿using CommonTestsUtilities.Requests;
+using FluentAssertions;
+using MyRecipes.Application.UseCases.User.Register;
+using MyRecipes.Exceptions;
+
+namespace Validators.Tests.User.Register;
+
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Error_Name_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.EndsWith(ResourceMessagesExceptions.NAME_EMPTY));
+    }
+
+    [Fact]
+    public void Error_Email_Empty()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.EndsWith(ResourceMessagesExceptions.EMAIL_EMPTY));
+    }
+
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = "email.com";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.EndsWith(ResourceMessagesExceptions.EMAIL_INVALID));
+    }
+
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    public void Error_Password_Invalid(int passwordLength)
+    {
+        var validator = new RegisterUserValidator();
+
+        var request = RequestRegisterUserJsonBuilder.Build(passwordLength);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.EndsWith(ResourceMessagesExceptions.PASSWORD_EMPTY));
+    }
+}

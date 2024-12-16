4b8462a4b4ecb992f7f42352232fbd412c7fe70a
Rafael Freitas <raffreitasx@outlook.com>
2024-12-16 20:53:01 -0300

    feat: adding controller to do login

---
+++ src/MyRecipes.API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipes.Application.UseCases.Login.DoLogin;
+using MyRecipes.Communication.Requests;
+using MyRecipes.Communication.Responses;
+
+namespace MyRecipes.API.Controllers;
+
+[Route("login")]
+public class LoginController : MyRecipesBaseController
+{
+    [HttpPost]
+    [ProducesResponseType<ResponseRegisteredUserJson>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ResponseErrorJson>(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        [FromServices] IDoLoginUseCase userCase,
+        [FromBody] RequestLoginJson request)
+    {
+        var response = await userCase.Execute(request);
+
+        return Ok(response);
+    }
+}

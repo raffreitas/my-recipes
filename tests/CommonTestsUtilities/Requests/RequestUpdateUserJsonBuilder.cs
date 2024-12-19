552fb2207f50ae59064ec4cf4217c7d225f3a6ca
Rafael Freitas <raffreitasx@outlook.com>
2024-12-19 19:22:44 -0300

    tests: adds update user usecase unit tests

---
+++ tests/CommonTestsUtilities/Requests/RequestUpdateUserJsonBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using MyRecipes.Communication.Requests;
+
+namespace CommonTestsUtilities.Requests;
+public class RequestUpdateUserJsonBuilder
+{
+    public static RequestUpdateUserJson Build()
+    {
+        return new Faker<RequestUpdateUserJson>()
+            .RuleFor(user => user.Name, f => f.Person.FirstName)
+            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Email));
+    }
+}
